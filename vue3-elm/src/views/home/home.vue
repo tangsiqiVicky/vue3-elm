eacbd579c781f3fad7c07956633d6f8fffdc196e
tangsiqiVicky <tsq282549373@163.com>
2019-01-10 09:28:25 +0800

    Update home.vue

--- vue3-elm/src/views/home/home.vue
+++ vue3-elm/src/views/home/home.vue
@@ -1,5 +1,15 @@
 <template>
-  <header-top :name="name"></header-top>
+  <div>
+  <header-top>
+    <span slot="logo" class="head_logo">ele.me</span>
+  </header-top>
+  <nav class="city_nav">
+    <div class="city_tip">
+      <span>当前定位城市:</span>
+      <span>定位不准时，请在城市列表中选择</span>
+    </div>
+  </nav>
+  </div>
 </template>
 
 <script>
@@ -8,7 +18,6 @@ export default {
   name: 'home',
   data () {
     return {
-      name: 'home'
     }
   },
   components: {
@@ -19,5 +28,24 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+@import 'src/style/mixin';
+  .head_logo {
+    left: 0.4em;
+    font-weight:400;
+    @include sc(0.7em, #fff);
+    @include ct;
+  }
+  .city_nav {
+    padding-top: 2.35em;
+    border-top: 1px solild $bc;
+    background-color: #fff;
+    margin-bottom: 0.4em;
+    .city_tip {
+      @include fj;
+      line-height: 1.45em;
+      padding: 0 0.45em;
+      
 
+    }
+  }
 </style>
